bacc4dbb629710a991831d549698dfe49039f808
28vignesh28 <vigneshganisetty@email.com>
2023-11-23 16:48:12 +0530

    icon slider

--- urban-company/script.js
+++ urban-company/script.js
@@ -1,16 +1,19 @@
-
-// var slideindex=1;
-// showdivs(1);
-// function plusDivs(n){
-//     showdivs(slideindex +=n);
-// }
-// function showdivs(n){
-//     var i;
-//     var x=document.getElementsByClassName("myslides");
-//     if(n>x.length){slideindex=1}
-//     if(n<1){slideindex=x.length}
-//     for(i=0;i<x.length;i++){
-//         x[i].style.display="none";
-//     }
-//     x[slideindex-1].style.display ="block";
-// }
+var slideindex = 1;
+showdivs(1);
+function plusDivs(n) {
+  showdivs((slideindex += n));
+}
+function showdivs(n) {
+  var i;
+  var x = document.getElementsByClassName("myslides");
+  if (n > x.length) {
+    slideindex = 1;
+  }
+  if (n < 1) {
+    slideindex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideindex - 1].style.display = "block";
+}
